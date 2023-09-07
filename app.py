511787d9a2702c229bd193f5fc2c3b7ab0c83378
jaime <jwcs2@aluno.ifal.edu.br>
2023-09-06 21:18:54 -0300

    finalizo por hoje

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+# import request
 app = Flask(__name__)
 
-@app.route("/")
+
+
+@app.route("/home")
 def home():
-    return render_template("cadastro.html")
+    return render_template("home.html")
 
 @app.route("/cadastro")
 def cadastro():
     return render_template('cadastro.html')
 
 
-@app.route("/login")
+usuarios= {
+    "Jaime": "senha", 
+    "Willian": "123456", 
+    "Hendryk": "Coxinha"
+}
+@app.route("/")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        usuario = request.form.get("usuario")
+        senha = request.form.get("senha")
+
+        # if usuario is None:
+        #     return render_template("login", mensagem = "Inserir usuário é brigatório;")
+        if usuario not in usuarios.keys():
+            return render_template("login.html", mensagem= "O usuário e/ou senha incorreto  usuaro")
+
+        user_password = usuarios[usuario]
+        if senha!= user_password:
+            return render_template("login.html", mensagem= "O usuário e/ou senha incorreto")
+        
+        return render_template("home.html" , user = usuario)
+
+        
+
     return render_template('login.html')
+
+
 
 if __name__== "__main__":
     app.run(debug=True)
